11e0700f78290a46b534837a48e75bc0c23f8347
admin <admin@glyonsmac.dhcp.tripadvisor.com>
2013-06-02 16:55:40 -0700

    real server and server merged

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ var handle = {}
 handle[""] = requestHandlers.index;
 handle["/"] = requestHandlers.index;
 
-server.start(router.route, handle);
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var postData = "";
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " recieved.");
+    route(handle, pathname, response, request);
+  }
+
+
+  http.createServer(onRequest).listen(8080);
+
+  console.log("Server has started.");
+}
+
+start(router.route, handle);
